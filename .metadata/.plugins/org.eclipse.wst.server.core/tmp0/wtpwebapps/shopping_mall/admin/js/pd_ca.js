bfa3b96ce222b8d9258c235a7fc673185041e0fb
5ore5o <5ore5o@naver.com>
2024-08-16 17:20:49 +0900

    cackall해결하자

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/shopping_mall/admin/js/pd_ca.js
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/shopping_mall/admin/js/pd_ca.js
@@ -1,3 +1,5 @@
+var ca_idx= document.getElementsByName("ca_idx");
+var cackall= document.getElementById("cackall");
 export class caset{
 cateset(){
 	location.href="./cate_write.do";
@@ -20,6 +22,33 @@ catein(cacode_ck){
 	cawr_frm.action="./cate_in.do";
 	cawr_frm.submit();
 	}
+}
+cack_all(){
+var w=0;
+while(w<ca_idx.length){
+	ca_idx[w].checked=cackall.checked;
+	w++;
+}
+}
+ca_del(){
+var w=0;
+var count=0;
+while(w<ca_idx.length){
+	if(ca_idx[w].checked==true){
+		count++;
+	}
+	w++;
+}
+if(count>0){
+	if(confirm("해당 데이터를 삭제하겠습니까?(단, 등록된 상품이 있을 경우 삭제 X)")){
+		cateset_frm.method="post";
+		cateset_frm.action="./del_cateck.do";
+		cateset_frm.submit();
+	}
+}
+else{
+	alert("선택한 데이터가 1개 이상 체크되어야 합니다.");
+}	
 }
 }
 
